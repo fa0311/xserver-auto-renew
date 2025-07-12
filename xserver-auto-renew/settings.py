d7ac72d8f2a932d33043d6d524fd8934b6f70a6e
ふぁ <yuki@yuki0311.com>
2025-07-13 00:29:34 +0900

    Revert "add login"
    This reverts commit 0d50d30a6c0c50f8cb7b804ae8c9c3a68c66febb.
    
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- xserver-auto-renew/settings.py
+++ xserver-auto-renew/settings.py
@@ -9,13 +9,3 @@ class Settings(BaseSettings):
 
     def __init__(self):
         super().__init__()
-
-
-class LoginSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-
-    username: str = Field()
-    password: str = Field()
-
-    def __init__(self):
-        super().__init__()
